da12c6c9a956b289ddd29e573b366355ee46255f
Derrick D. Cochran <derrick@cochran.design>
2026-06-12 09:04:30 -0500

    v1.1.0

--- scripts/packs.mjs
+++ scripts/packs.mjs
@@ -17,7 +17,7 @@
 export class RokuganPacks {
 
   /** Bump to force a reseed on the next world load. */
-  static DATA_VERSION = "1.0.0";
+  static DATA_VERSION = "1.1.0";
 
   static PACKS = ["classes", "classfeatures", "species", "backgrounds", "feats",
                   "equipment", "techniques", "invocations"];
